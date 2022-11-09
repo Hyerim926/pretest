4b7178a01849a98a875ad04add8fa0c2744274e5
Hyerim926 <ekfrhsk96@icloud.com>
2022-11-10 00:10:39 +0900

    docs: Add sql queries for set database and tables

---
+++ dbinit.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS `inflab` DEFAULT CHARACTER SET utf8;
+USE `inflab`;
+
+CREATE TABLE IF NOT EXISTS `teacher` (
+    `id` int(11) not null auto_increment,
+    `name` varchar(20) not null,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `lecture` (
+    `id` int(11) not null auto_increment,
+    `name` varchar(20) not null,
+    `category` varchar(10) not null,
+    `teacher_id` int(11) not null,
+    `introduction` text,
+    `fee` int(11) not null,
+    `s_open` char not null default 'N',
+    `s_delete` char not null default 'N',
+    `created_at` datetime default now(),
+    `updated_at` datetime,
+    PRIMARY KEY primary_lecture_id (`id`),
+    FOREIGN KEY foreign_lecture_teacherId (`teacher_id`) REFERENCES teacher(`id`),
+    UNIQUE KEY unique_lecture_name (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `student` (
+    `id` int(11) not null auto_increment,
+    `nickname` varchar(20) not null,
+    `email` varchar(20) not null,
+    `s_withdraw` char not null default 'N',
+    `created_at` datetime default now(),
+    `updated_at` datetime,
+    PRIMARY KEY primary_student_id (`id`),
+    UNIQUE KEY unique_student_email (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `enrolment` (
+    `id` int(11) not null auto_increment,
+    `lecture_id` int(11) not null,
+    `student_id` int(11) not null,
+    `created_at` datetime default now(),
+    `updated_at` datetime,
+    PRIMARY KEY primary_enrolment_id (`id`),
+    FOREIGN KEY foreign_enrolment_lectureId (`lecture_id`) REFERENCES lecture(`id`),
+    FOREIGN KEY foreign_enrolment_studentId (`student_id`) REFERENCES student(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
