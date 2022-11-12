b344cd59f186412cf34ddabc834f3a639552b289
Hyerim926 <ekfrhsk96@icloud.com>
2022-11-12 23:02:18 +0900

    chore: Add init queries

--- dbinit.sql
+++ dbinit.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `lecture` (
     `fee` int(11) not null,
     `s_open` char not null default 'N',
     `s_delete` char not null default 'N',
-    `created_at` datetime default now(),
+    `created_at` datetime not null default now(),
     `updated_at` datetime,
     PRIMARY KEY primary_lecture_id (`id`),
     FOREIGN KEY foreign_lecture_teacherId (`teacher_id`) REFERENCES teacher(`id`),
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `student` (
     `nickname` varchar(20) not null,
     `email` varchar(20) not null,
     `s_withdraw` char not null default 'N',
-    `created_at` datetime default now(),
+    `created_at` datetime not null default now(),
     `updated_at` datetime,
     PRIMARY KEY primary_student_id (`id`),
     UNIQUE KEY unique_student_email (`email`)
@@ -38,9 +38,20 @@ CREATE TABLE IF NOT EXISTS `enrolment` (
     `id` int(11) not null auto_increment,
     `lecture_id` int(11) not null,
     `student_id` int(11) not null,
-    `created_at` datetime default now(),
+    `s_delete` char not null default 'N',
+    `created_at` datetime not null default now(),
     `updated_at` datetime,
     PRIMARY KEY primary_enrolment_id (`id`),
     FOREIGN KEY foreign_enrolment_lectureId (`lecture_id`) REFERENCES lecture(`id`),
     FOREIGN KEY foreign_enrolment_studentId (`student_id`) REFERENCES student(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO TEACHER (NAME) SELECT '김도영' FROM dual WHERE NOT EXISTS(
+    SELECT * FROM TEACHER WHERE NAME = '김도영'
+    );
+INSERT INTO TEACHER (NAME) SELECT '정재현' FROM dual WHERE NOT EXISTS(
+    SELECT * FROM TEACHER WHERE NAME = '정재현'
+    );
+INSERT INTO TEACHER (NAME) SELECT '김정우' FROM dual WHERE NOT EXISTS(
+    SELECT * FROM TEACHER WHERE NAME = '김정우'
+    );
